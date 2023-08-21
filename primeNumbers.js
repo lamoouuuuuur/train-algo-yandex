2dc5111de95f87c43b2fec23937a902824bf5414
lamoouuuuuur <nastyasavi9a@gmail.com>
2023-08-21 20:58:35 +0300

    add parentheses

---
+++ primeNumbers.js
@@ -0,0 +1,11 @@
+const primeNumbers = (n) => {
+    const result = [];
+
+    for (let i = 2; i <= n; i++) {
+        result.push(i);
+    }
+
+    for (let j = 0; j < result.length; j++) {
+
+    }
+}
