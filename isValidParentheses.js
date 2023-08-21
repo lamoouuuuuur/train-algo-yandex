50b9186dbd884a5c85cadb7f761731ac7b3d606f
lamoouuuuuur <nastyasavi9a@gmail.com>
2023-08-21 20:58:51 +0300

    add parentheses

--- isValidParentheses.js
+++ isValidParentheses.js
@@ -4,10 +4,25 @@ const pairs = {
     "{": "}"
 }
 
+const stringStartChecking = (s) => {
+    return s[0] === ")" || s[0] === "]" || s[0] === "}";
+}
+
+const stringEndChecking = (s) => {
+    const stringLength = s.length;
+    return s[stringLength - 1] === "(" || s[stringLength - 1] === "[" || s[stringLength - 1] === "{"
+}
+
+const isOddLengthOfString = (s) => {
+    return s.length % 2 === 1;
+}
+
+
 const isValidParentheses = (s) => {
-    if (s.length % 2 === 1) return false;
-    if (s[0] === ')' || s[0] === ']' || s[0] === "}") return false;
-    if (s[s.length - 1] === "(" || s[s.length - 1] === "[" || s[s.length - 1] === "{") return false;
+
+    isOddLengthOfString(s);
+    stringStartChecking(s);
+    stringEndChecking(s);
 
     const stack = [];
 
