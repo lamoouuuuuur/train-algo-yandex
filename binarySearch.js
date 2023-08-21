d5df98c70aadfe476c865f9d51cee3d922d34242
lamoouuuuuur <nastyasavi9a@gmail.com>
2023-08-21 21:14:40 +0300

    add binary search

---
+++ binarySearch.js
@@ -0,0 +1,22 @@
+const binarySearch = (nums, target) => {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2);
+
+        if (nums[middle] === target) {
+            return middle;
+        } else if (nums[middle] > target) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+
+    return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 1))
+console.log(binarySearch([1, 2, 3, 4], 3))
+console.log(binarySearch([1, 2, 3, 7, 4], 7))
